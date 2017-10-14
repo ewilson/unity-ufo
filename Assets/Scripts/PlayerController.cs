8e6d6ac1dbbec00166224c39d4a19cf6c9b59770
Eric Wilson <wilson.eric.n@gmail.com>
2017-10-14 19:57:25 -0400

    finished?

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+	public Text countText;
+	public Text winText;
 
 	private Rigidbody2D rb2d;
+	private int count;
 
 	void Start()
 	{
+		count = 0;
+		SetCountText ();
+		winText.text = "";
 		rb2d = GetComponent<Rigidbody2D> ();
 	}
 
@@ -20,5 +27,24 @@ public class PlayerController : MonoBehaviour {
 		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
 		rb2d.AddForce (movement * speed);
 
+	}
+
+	void OnTriggerEnter2D(Collider2D other) 
+	{
+		if (other.gameObject.CompareTag("PickUp"))
+		{
+			count += 1;
+			SetCountText ();
+			other.gameObject.SetActive (false);
+			if (count == 12) 
+			{
+				winText.text = "You win!!";
+			}
+		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString ();
 	}
 }
